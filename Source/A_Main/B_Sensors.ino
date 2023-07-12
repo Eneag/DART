ea8f8e174783cadc8cb61c4facab62456b7c9588
Eneag <eneawsio@gmail.com>
2023-07-12 18:11:59 +0200

    Structure of functions

---
+++ Source/A_Main/B_Sensors.ino
@@ -0,0 +1,33 @@
+int readAcc(char charAxis) { //reads a char, either 'X', 'Y' or 'Z', and returns the Accellerometer value in that axis
+ TBD
+ //example of how i'd do it
+ /*
+  int acc;
+  int axis;
+
+  if (charAxis == 'X') {
+    axis = ACC_X_PIN;  //o qualcosa del genere
+  }
+  else if(charAxis == 'Y'){
+    axis = ACC_Y_PIN;  //
+  }else{
+    axis = ACC_Z_PIN;
+  }
+
+  //buh magia nera 
+
+  return acc;
+  */
+}
+
+int readAng(char axis) { //reads a char, either 'X', 'Y' or 'Z', and returns the Accellerometer value in that axis
+  TBD
+}
+
+int readBar() { //returns the Barometer value or height
+  TBD
+}
+
+int readTemp() { //returns Termometer value
+  TBD
+}
